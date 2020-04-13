ad24b3e9f68d89811f33439464c5962b74ba6c32
zijianguan0204 <31665442+zijianguan0204@users.noreply.github.com>
2020-04-13 11:07:30 -0700

    update

--- app.py
+++ app.py
@@ -1,59 +1,13 @@
-from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm, CalculateForm
+from flask import Flask, render_template, flash
+from forms import CalculateForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts=posts)
-
-
-@app.route("/about")
-def about():
-    return render_template('about.html', title='About')
-
-
-@app.route("/register", methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
-    return render_template('register.html', title='Register', form=form)
-
-
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
-
-
-@app.route("/calculate", methods=['GET', 'POST'])
-def calculate():
     proceeds = 0
     cost = 0
     net_profit = 0
@@ -69,16 +23,16 @@ def calculate():
         buy_commission = form.buy_commission.data
         tax = form.tax.data
 
-        proceeds = allotment*final_sell_price
-        cost = allotment*initial_sell_price+buy_commission+sell_commission+tax*proceeds
+        proceeds = allotment * final_sell_price
+        cost = allotment * initial_sell_price + buy_commission + sell_commission + tax * proceeds
         net_profit = proceeds - cost
-        return_on_inv = net_profit/cost
-        break_piece = net_profit/allotment
+        return_on_inv = net_profit / cost
+        break_piece = net_profit / allotment
         print(tax)
     else:
         flash('calculator failed')
-    return render_template('calculator.html', title='Calculator', form=form, proceeds=proceeds, cost=cost, net_profit=net_profit, return_on_inv=return_on_inv, break_piece=break_piece)
-
+    return render_template('calculator.html', title='Calculator', form=form, proceeds=proceeds, cost=cost,
+                           net_profit=net_profit, return_on_inv=return_on_inv, break_piece=break_piece)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
