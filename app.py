dc6ca0b1e00fa304aabd923617d606c736340245
zijianguan0204 <31665442+zijianguan0204@users.noreply.github.com>
2020-04-13 02:27:46 -0700

    first init

---
+++ app.py
@@ -0,0 +1,79 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm, CalculateForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/calculate", methods=['GET', 'POST'])
+def calculate():
+    form = CalculateForm()
+    if form.validate_on_submit():
+        symbol = form.symbol.data
+        allotment = form.allotment.data
+        final_sell_price = form.final_share_price.data
+        sell_commission = form.sell_commission.data
+        initial_sell_price = form.initial_share_price.data
+        buy_commission = form.buy_commission.data
+        tax = form.tax.data
+
+        proceeds = allotment*final_sell_price
+        cost = allotment*initial_sell_price+buy_commission+sell_commission+tax*proceeds
+        net_profit = proceeds - cost
+        return_on_inv = net_profit/cost
+        break_piece = net_profit/allotment
+        print(tax)
+    else:
+        flash('calculator failed')
+    return render_template('calculator.html', title='Calculator', form=form, proceeds=proceeds, cost=cost, net_profit=net_profit, return_on_inv=return_on_inv, break_piece=break_piece)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ forms.py
@@ -0,0 +1,34 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, FloatField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+
+class CalculateForm(FlaskForm):
+    symbol = StringField('Stock Symbol',
+                         validators=[DataRequired()])
+    allotment = IntegerField('Allotment', validators=[DataRequired()])
+    final_share_price = IntegerField('Final Share Price', validators=[DataRequired()])
+    sell_commission = IntegerField('Sell Commission', validators=[DataRequired()])
+    initial_share_price = IntegerField('Initial Share Price', validators=[DataRequired()])
+    buy_commission = IntegerField('Final Share Price', validators=[DataRequired()])
+    tax = FloatField('Tax', validators=[DataRequired()])
+    submit = SubmitField('Calculate')
