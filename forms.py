1952d1f196539a200fa4dc36ea8d763f36f7a141
zijianguan0204 <31665442+zijianguan0204@users.noreply.github.com>
2020-05-16 00:51:47 -0700

    update on amount checker

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectField, StringField, PasswordField, SubmitField, BooleanField, IntegerField, FloatField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
 class CalculateForm(FlaskForm):
     symbol = StringField('Stock Symbol',
@@ -19,9 +19,15 @@ class realTimeInfoForm(FlaskForm):
     submit = SubmitField('Calculate')
 
 class invsForm(FlaskForm):
-    input_amount = IntegerField('Input Dollor Amount',validators=[DataRequired()])
+    def amount_check(form, field):
+            if field.data < 5000:
+                raise ValidationError('The input amount must bigger or equal to 5000')
+
+    input_amount = IntegerField('Input Dollor Amount',validators=[DataRequired(), amount_check])
     # invs_choices = [("Ethical Investing"), ("Growth Investing"), ("Index Investing"), ("Quality Investing"),("Value Investing")]
     # invs_method = SelectField(u'Investing Strategy', invs_choices)
     invs_method = StringField('Investing Strategy', validators=[DataRequired()])
     invs_method_opt = StringField('Second Investing Strategy')
     submit = SubmitField('Submit')
+
+    
